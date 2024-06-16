fd96087522ba33f178cd1065a1087329b7b1aa06
felipe.chagas <felypegt8@gmail.com>
2024-06-16 18:05:46 -0300

    modifying profile home

--- src/templates/Home/Home.jsx
+++ src/templates/Home/Home.jsx
@@ -2,9 +2,45 @@
 import './Home.css';
 
 function App() {
+  const handleContact = () => {
+    console.log('felipe');
+  }
   return (
     <>
-    <h1>I am Felipe Chagas</h1>
+    <section>
+      <nav>
+        <div>
+          <p>logo</p>
+        </div>
+        <div>
+          {/* <a href="#">about</a>
+          <a href="#">portfolio</a>
+          <a href="#">contact</a> */}
+        </div>
+        <div>
+          <input type="search" />
+        </div>
+      </nav>
+    </section>
+    <main>
+      <section>
+        <h1>I am <span>Felipe Chagas</span></h1>
+        <h2>Python and Reac Developer</h2>
+      </section>
+      <section>
+        <img src="#" alt="felipe chagas profile" />
+      </section>
+    </main>
+    <section>
+      <button onClick={handleContact}>contact</button>
+    </section>
+    <footer>
+      <ul>
+        <li>Linkedin</li>
+        <li>Instagram</li>
+        <li>Gmail</li>
+      </ul>
+    </footer>
     </>
   );
 }
